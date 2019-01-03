c045b92e917412921b8ab9ae06d52195ee7c8031
seanshen20 <34725930+seanshen20@users.noreply.github.com>
2019-01-03 11:08:38 +1100

    session 6: theme

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,25 +9,31 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
 
+  username: any;
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertifyService: AlertifyService) {
+  }
   
   ngOnInit() {
    
   }
 
   login() {
-    this.authService.login(this.model).subscribe(next =>  console.log('Logged in sucessfully')
-      , error => console.log('failed to login'))
-    console.log(this.model);
+    this.authService.login(this.model)
+      .subscribe(
+      next => {
+        this.alertifyService.success('Logged in sucessfully');
+      },
+      error => this.alertifyService.error(error))
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    this.username = this.authService.getName();
+    return this.authService.loggedIn();
   }
 
   loggedOut() {
     localStorage.removeItem('token');
+    this.alertifyService.message('logged out');
 }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,19 +12,18 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private altetify: AlertifyService) {  }
 
   ngOnInit() {
   }
   register() {
     this.authService.register(this.model).subscribe(next =>
-      console.log("registration sucessful"),
-      error => console.log(error));
+      this.altetify.success("registration sucessful"),
+      error => this.altetify.error(error))
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log("cancel");
   }
 
 }
